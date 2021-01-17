030ba9fd5fea3781f941cdf00de872aaee07be2e
HuzefaMotorwala <huzefamotorwala5225@outlook.com>
2021-01-17 14:53:43 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import marked from 'marked';
+import './style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const placeholder = `# Welcome to my React Markdown Previewer!
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`;
+
+marked.setOptions({
+    breaks: true
+  });
+const renderer=new marked.Renderer();
+
+function App(){
+    const [text,setText]=React.useState(placeholder);
+    return(
+        <div className="text-center">
+            <h1 className="p-4">My Markdown Previewer</h1>
+            <textarea
+                name="text"
+                id="text"
+                rows="10"
+                value={text}
+                className="textarea"
+                onChange={(e)=>setText(e.target.value)}
+            ></textarea>
+            <h3 className="mt-3">Output</h3>
+            <Preview markdown={text}/>
+        </div>
+    )
+};
+function Preview({markdown}){
+    return(
+        <div
+        dangerouslySetInnerHTML={{
+            __html: marked(markdown, { renderer: renderer })
+          }}
+          id="preview"
+        >
+        </div>
+    );
+}
+
+ReactDOM.render(<App/>,document.getElementById("root"));
+
